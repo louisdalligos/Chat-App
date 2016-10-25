191aad94660ece633e3be92e068c0d3728411819
Louis Dalligos <louis@wepro.co>
2016-10-25 09:18:41 +0800

    Setup ui screens

---
+++ Chat App/LoginController.swift
@@ -0,0 +1,33 @@
+//
+//  LoginController.swift
+//  Chat App
+//
+//  Created by Louis on 25/10/2016.
+//  Copyright © 2016 Wepro Co. Ltd. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor(r: 61, g: 91, b: 151)
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+
+}
+
+
+extension UIColor {
+    
+    convenience init(r: CGFloat, g: CGFloat, b: CGFloat) {
+        self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
+    }
+    
+}
