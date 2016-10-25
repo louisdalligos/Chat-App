778ae5ab3efbc1bdecd92e47c27a3c11763eeccc
Louis Dalligos <louis@wepro.co>
2016-10-25 12:50:26 +0800

    Setup inputs container

--- Chat App/LoginController.swift
+++ Chat App/LoginController.swift
@@ -13,7 +13,20 @@ class LoginController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = UIColor(r: 61, g: 91, b: 151)
+        view.backgroundColor = UIColor(r: 119, g: 24, b: 215)
+        
+        let inputContainerView = UIView()
+        inputContainerView.backgroundColor = UIColor.white
+        inputContainerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // instantiate the input viewer
+        view.addSubview(inputContainerView)
+        
+        // add constraint of inputsContainerView - x, y, width and height
+        inputContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        inputContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        inputContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
+        inputContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
