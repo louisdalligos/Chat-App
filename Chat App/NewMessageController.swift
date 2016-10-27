a81adb729c7c7731aa5deb415f1e1473532a4f01
Louis Dalligos <louis@wepro.co>
2016-10-27 11:02:08 +0800

    Fetch and display user's name from Firebase

--- Chat App/NewMessageController.swift
+++ Chat App/NewMessageController.swift
@@ -7,17 +7,60 @@
 //
 
 import UIKit
+import Firebase
 
 class NewMessageController: UITableViewController {
+    
+    let cellID = "cellID"
+    var users = [User]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(handleCancel))
+        
+        fetchUser()
+    }
+    
+    func fetchUser() {
+        FIRDatabase.database().reference().child("users").observe(.childAdded, with: { (snapshot) in
+            
+            if let dictionary = snapshot.value as? [String: AnyObject] {
+                
+                let user = User()
+                
+                user.setValuesForKeys(dictionary)
+                self.users.append(user)
+                
+                self.tableView.reloadData()
+                
+                //print(user.name, user.email)
+            }
+            
+            
+            //print("User found")
+            //print(snapshot)
+            
+            }, withCancel: nil)
     }
     
     func handleCancel() {
         dismiss(animated: true, completion: nil)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // hack for now - use deque for cells later
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellID)
+        
+        let user = users[indexPath.row]
+        cell.textLabel?.text = user.name
+        
+        return cell
     }
 
 }
---
+++ Chat App/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  Chat App
+//
+//  Created by Louis on 27/10/2016.
+//  Copyright © 2016 Wepro Co. Ltd. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+    
+    var name: String?
+    var email: String?
+
+}
